0eee35f6776f8279a7f0297fb0eded4ec655b282
Levent YALCIN <leventyalcin@gmail.com>
2016-02-04 14:31:32 +0000

    delete cache files if there is internet connection

--- search.sh
+++ search.sh
@@ -21,7 +21,7 @@ queryJql=`echo $queryJql | sed s/{query}/$query/g`
 filename="cache/jira-$query.txt"
 
 # Remove files older than five minutes
-rm `find cache -mmin +${cacheTTLinMins}`
+rm `ping -c 1 -t 1 8.8.8.8 && find cache -mmin +${cacheTTLinMins}`
 
 # Create directory if required
 mkdir -p cache
